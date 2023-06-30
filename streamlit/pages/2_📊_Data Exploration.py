22ef647d9a9421534e002e8c4dffcefaa0c9098d
AnthonyLebret <121168615+AnthonyLebret@users.noreply.github.com>
2023-06-30 14:50:42 +0200

    Update 2_📊_Data Exploration.py

--- streamlit/pages/2_📊_Data Exploration.py
+++ streamlit/pages/2_📊_Data Exploration.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly_express as px
 import matplotlib.pyplot as plt
+import numba
 import umap
 
 st.set_page_config(page_title="Data Exploration", page_icon="📊")
